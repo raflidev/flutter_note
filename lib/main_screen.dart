ba9f2a8772d831c8eaf5c01c661763ffb08791ba
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-06-25 15:19:35 +0700

    responsive flutter

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -6,60 +6,127 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Note',
+        appBar: AppBar(
+          title: Text(
+            'Note',
+          ),
         ),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final NoteData data = NoteDataList[index];
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth <= 790) {
+              return NoteMobilePage();
+            } else {
+              return NoteWebPage();
+            }
+          },
+        ));
+  }
+}
 
-          return Center(
-            child: Container(
-              width: 1200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return NoteScreen(data: data);
-                          },
-                        ),
-                      );
+class NoteWebPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: GridView.count(
+        crossAxisCount: 4,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        children: NoteDataList.map(
+          (note) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return NoteScreen(data: note);
                     },
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 32,
-                          vertical: 32,
+                  ),
+                );
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 32,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        note.title,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800,
+                          fontFamily: 'Staatliches',
                         ),
-                        child: Row(
-                          children: [
-                            Text(
-                              data.title,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Staatliches',
-                              ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
+
+class NoteMobilePage extends StatelessWidget {
+  const NoteMobilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final NoteData data = NoteDataList[index];
+
+        return Center(
+          child: Container(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return NoteScreen(data: data);
+                        },
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 32,
+                        vertical: 32,
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            data.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: 'Staatliches',
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        },
-        itemCount: NoteDataList.length,
-      ),
+          ),
+        );
+      },
+      itemCount: NoteDataList.length,
     );
   }
 }
